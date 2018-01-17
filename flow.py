59cbd819d82e30401e8c905cfd69806b436aa2cc
lyd1ng <lyd1ng@protonmail.com>
2018-01-18 00:22:34 +0100

    Added flow.py the simulation backbone for waveguide_funnel.py

---
+++ flow.py
@@ -0,0 +1,54 @@
+from gplot_vector3 import gplot_vector3
+from h_modes import h_vector, e_vector, scale_vector
+
+
+# Discard every particle outside the boundaries and append them
+# to a seperated list
+# This way only particles inside the waveguide are updated
+def discard_particles(particles, discarded_particles, x_min, x_max,
+        y_min, y_max, z_min, z_max):
+    for i in range(0, len(particles)):
+        if (particles[i].x < x_min or particles[i].x > x_max or
+            particles[i].y < y_min or particles[i].y > y_max or
+            particles[i].z < z_min or particles[i].z > z_max):
+                # Remove particle from particles and add it
+                # to discarded_particles
+                discarded_particles.append(particles[i])
+    # Now we have to delete every discarded particle from
+    # particle list
+    for dp in discarded_particles:
+        if dp in particles:
+            particles.remove(dp)
+
+
+# Move every particle (gplot_vector3) in particles along
+# the e field at fix t
+# This way particles hopefully accumulate at e field vortices
+def funnel_particles_e_field(particles, discarded_particles, iterations,
+        speed, a, b, m, n, omega, lambda_mn, x_min, x_max, y_min, y_max,
+        z_min, z_max, t):
+    for i in range(0, iterations):
+        discard_particles(particles, discarded_particles, x_min, x_max,
+            y_min, y_max, z_min, z_max)
+        for p in particles:
+            v = e_vector(a, b, m, n, omega, lambda_mn, p.x, p.y, p.z, t)
+            scale_vector(v, speed, speed, speed)
+            p.x += v.x
+            p.y += v.y
+            p.z += v.z
+
+# Move every particle (gplot_vector3) in particles along
+# the h field at fix t
+# This way particles hopefully accumulate at h field vortices
+def funnel_particles_h_field(particles, discarded_particles, iterations,
+        speed, a, b, m, n, omega, lambda_mn, x_min, x_max, y_min, y_max,
+        z_min, z_max, t):
+    for i in range(0, iterations):
+        discard_particles(particles, discarded_particles, x_min, x_max,
+            y_min, y_max, z_min, z_max)
+        for p in particles:
+            v = h_vector(a, b, m, n, omega, lambda_mn, p.x, p.y, p.z, t)
+            scale_vector(v, speed, speed, speed)
+            p.x += v.dx.real
+            p.y += v.dy.real
+            p.z += v.dz.real
