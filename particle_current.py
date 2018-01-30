449a6394a9c9253cd77ab0f79b102551711bec19
lyd1ng <lyd1ng@protonmail.com>
2018-01-30 22:50:09 +0100

    Made particle_current.py pep8 compatible

--- particle_current.py
+++ particle_current.py
@@ -1,4 +1,3 @@
-from gplot_vector3 import gplot_vector3
 from h_modes import h_vector, e_vector, scale_vector
 
 
@@ -9,11 +8,11 @@ def discard_particles(particles, x_min, x_max,
     discarded_particles = list()
     for i in range(0, len(particles)):
         if (particles[i].x < x_min or particles[i].x > x_max or
-            particles[i].y < y_min or particles[i].y > y_max or
-            particles[i].z < z_min or particles[i].z > z_max):
-                # Remove particle from particles and add it
-                # to discarded_particles
-                discarded_particles.append(particles[i])
+                particles[i].y < y_min or particles[i].y > y_max or
+                particles[i].z < z_min or particles[i].z > z_max):
+            # Remove particle from particles and add it
+            # to discarded_particles
+            discarded_particles.append(particles[i])
     # Now we have to delete every discarded particle from
     # particle list
     for dp in discarded_particles:
@@ -26,7 +25,7 @@ def discard_particles(particles, x_min, x_max,
 # This way particles hopefully accumulate at e field vortices
 def funnel_particles_e_field(args):
     particles, speed, a, b, m, n, omega, lambda_mn, x_min, x_max, y_min,\
-            y_max, z_min, z_max, t = args
+        y_max, z_min, z_max, t = args
     discard_particles(particles, x_min, x_max,
     y_min, y_max, z_min, z_max)
     for p in particles:
@@ -40,12 +39,13 @@ def funnel_particles_e_field(args):
         p.dz = v.dz.real
     return particles
 
+
 # Move every particle (gplot_vector3) in particles along
 # the h field at fix t
 # This way particles hopefully accumulate at h field vortices
 def funnel_particles_h_field(args):
     particles, speed, a, b, m, n, omega, lambda_mn, x_min, x_max, y_min,\
-            y_max, z_min, z_max, t = args
+        y_max, z_min, z_max, t = args
     # Check for particles outsite the waveguide
     discard_particles(particles, x_min, x_max,
         y_min, y_max, z_min, z_max)
